5ca83608490eed5a629c3d9babe4a6a4d65068df
codeisgood21 <keshav9410070139@gmail.com>
2026-06-17 22:16:16 +0000

    basics

---
+++ basicsofjs/datatypes.js
@@ -0,0 +1,19 @@
+//alert("hello") we are using nodejs not browser
+console.log(3+3) 
+let name ="pratham";
+let age  = 18
+let bool = true 
+let state = null // abhi khali hai !
+// number => 2 to power 53
+//bigint 
+//string =>""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+//object 
+console.log(typeof(age))
+console.log(typeof(state)) //null ek object hai and undefined undefined hi hai 
+
+
+
---
+++ basicsofjs/var.js
@@ -0,0 +1,8 @@
+const id =121232
+let accemail = "pratham@google.com" 
+var accpassword = "3e23"// never use var bcz os issue in block scope and functional scope
+acccity = "jaipur"
+// const cannot be changed
+let number ;
+console.log(id);
+console.table([id,accemail,accpassword,acccity,number]) // tabls create krne ke liye 
