c185ab59e5de216b46d142aa157f8051ba3603a8
codeisgood21 <keshav9410070139@gmail.com>
2026-06-18 23:08:25 +0000

    commit

--- basicsofjs/date_time.js
+++ basicsofjs/date_time.js
@@ -1,14 +1,16 @@
-//dates
- let mydate = new Date()
- console.log(mydate)
- console.log(mydate.toDateString())
- console.log(typeof mydate)
- let date = new Date(2023,0,23,1,4)
- console.log(date.toDateString())
- let mytime = Date.now()
- console.log(mytime)
-date.toLocaleDateString('default',{
-    weekday :"long ",
-})
-console.log(date)
+// //dates
+//  let mydate = new Date()
+//  console.log(mydate)
+//  console.log(mydate.toDateString())
+//  console.log(typeof mydate)
+//  let date = new Date(2023,0,23,1,4)
+//  console.log(date.toDateString())
+//  let mytime = Date.now()
+//  console.log(mytime)
+// date.toLocaleDateString('default',{
+//     weekday :"long ",
+// })
+// console.log(date)
 
+const mytime = new Date()
+console.log(mytime.toLocaleTimeString())
