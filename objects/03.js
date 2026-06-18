4256427336cef37eb3f8b98824d7c8f51def7aff
codeisgood21 <keshav9410070139@gmail.com>
2026-06-18 13:41:28 +0000

    commit

--- objects/03.js
+++ objects/03.js
@@ -1,11 +1,11 @@
-//api value in json 
-{
-    "name":"Pratham",
-   " age": 23,
-   " price" :"Free"
-}
-// api responsae in array jisme json objects hai 
-[
-    {},
-    {},
-]
+// //api value in json 
+// {
+//     "name":"Pratham",
+//    " age": 23,
+//    " price" :"Free"
+// }
+// // api responsae in array jisme json objects hai 
+// [
+//     {},
+//     {},
+// ]
